c09da4e0f54cbdb7d734e11725bc20838ae58e6d
Ubuntu <momoka@hongo.wide.ad.jp>
2022-05-12 19:54:37 +0900

    change

---
+++ 2HE.cpp
@@ -0,0 +1,12 @@
+// g++ -std=c++14 2HE.cpp -lpthread
+#include <arpa/inet.h>
+#include <condition_variable>
+#include <errno.h>
+#include <iostream>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <thread>
+#include <unistd.h>
--- HE.cpp
+++ HE.cpp
@@ -31,12 +31,15 @@ enum class State {
 struct Notification {
   std::mutex m;
   State status = State::WaitingBoth;
-  char addr[256];
+  struct addrinfo *addrlist;
+  struct addrinfo *addrlist_IPv4;
+  struct addrinfo *addrlist_IPv6;
   std::condition_variable cv;
 };
 
+
 void ipv4proc(Notification &notif, const std::chrono::milliseconds &waitTime,
-              char *hostname) {
+              const char *hostname) {
   puts("request A");
   std::this_thread::sleep_for(waitTime);
   {
@@ -44,22 +47,19 @@ void ipv4proc(Notification &notif, const std::chrono::milliseconds &waitTime,
     if (notif.status == State::WaitingBoth) {
       // notif.addr = 1234;
       struct addrinfo hints;
-      struct addrinfo *res;
       char host[256];
 
       memset(&hints, 0, sizeof(struct addrinfo));
       hints.ai_family = AF_INET;
       hints.ai_socktype = SOCK_STREAM;
 
-      int ret = getaddrinfo("google.com", NULL, &hints, &res);
+      int ret = getaddrinfo(hostname, "80", &hints, &notif.addrlist_IPv4);
       if (ret != 0) {
         fprintf(stderr, "getaddrinfo IPv4: %s\n", gai_strerror(ret));
         // exit(EXIT_FAILURE);
         return;
       }
-      getnameinfo(res->ai_addr, res->ai_addrlen, notif.addr, sizeof(notif.addr),
-                  NULL, 0, NI_NUMERICHOST);
-
+      // addIPv4list(notif.addrlist, notif.addrlist_IPv4);
       notif.status = State::WaitingIpv6;
     }
   }
@@ -68,7 +68,7 @@ void ipv4proc(Notification &notif, const std::chrono::milliseconds &waitTime,
 }
 
 void ipv6proc(Notification &notif, const std::chrono::milliseconds &waitTime,
-              char *hostname) {
+              const char *hostname) {
   puts("request AAAA");
   std::this_thread::sleep_for(waitTime);
   {
@@ -76,22 +76,19 @@ void ipv6proc(Notification &notif, const std::chrono::milliseconds &waitTime,
     if (notif.status == State::WaitingBoth ||
         notif.status == State::WaitingIpv6) {
       struct addrinfo hints;
-      struct addrinfo *res;
       char host[256];
 
       memset(&hints, 0, sizeof(struct addrinfo));
       hints.ai_family = AF_INET6;
       hints.ai_socktype = SOCK_STREAM;
 
-      int ret = getaddrinfo(hostname, NULL, &hints, &res);
+      int ret = getaddrinfo(hostname, "80", &hints, &notif.addrlist_IPv6);
       if (ret != 0) {
         fprintf(stderr, "getaddrinfo IPv6: %s\n", gai_strerror(ret));
         // exit(EXIT_FAILURE);
         return;
       }
-      getnameinfo(res->ai_addr, res->ai_addrlen, notif.addr, sizeof(notif.addr),
-                  NULL, 0, NI_NUMERICHOST);
-
+      // addIPv6list(notif.addrlist, notif.addrlist_IPv6);
       notif.status = State::Ipv6;
     }
   }
@@ -100,7 +97,7 @@ void ipv6proc(Notification &notif, const std::chrono::milliseconds &waitTime,
 }
 
 void happyEyeball2(const std::chrono::milliseconds waitTime1,
-                   const std::chrono::milliseconds waitTime2, char *hostname) {
+                   const std::chrono::milliseconds waitTime2,const  char *hostname) {
   std::cout << "test happyEyeball2 time:" << waitTime1.count() << "ms "
             << waitTime2.count() << "ms" << std::endl;
   Notification notif;
@@ -128,34 +125,80 @@ void happyEyeball2(const std::chrono::milliseconds waitTime1,
       }
     }
   }
+  struct addrinfo *tmp;
+  char host[256];
+  int fd = -1;
   switch (notif.status) {
   case State::Ipv4:
     puts("ipv4 syn");
+    for (tmp = notif.addrlist_IPv4; tmp != NULL; tmp = tmp->ai_next) {
+      getnameinfo(tmp->ai_addr, tmp->ai_addrlen, host, sizeof(host), NULL, 0,
+                  NI_NUMERICHOST);
+      puts(host);
+    }
+    for (tmp = notif.addrlist_IPv4; tmp != NULL; tmp = tmp->ai_next) {
+      printf("start connect IPv4 \n");
+      fd = socket(tmp->ai_family, tmp->ai_socktype, tmp->ai_protocol);
+      if (fd == -1) {
+        continue;
+      }
+      if (connect(fd, tmp->ai_addr, (socklen_t)tmp->ai_addrlen) != 0) {
+        fprintf(stderr, "connect: %s\n", strerror(errno));
+      } else {
+        printf("connected to ");
+        getnameinfo(tmp->ai_addr, tmp->ai_addrlen, host, sizeof(host), NULL, 0,
+                  NI_NUMERICHOST);
+        puts(host);
+      }
+      break;
+    }
     break;
   case State::Ipv6:
     puts("ipv6 syn");
+    for (tmp = notif.addrlist_IPv6; tmp != NULL; tmp = tmp->ai_next) {
+      getnameinfo(tmp->ai_addr, tmp->ai_addrlen, host, sizeof(host), NULL, 0,
+                  NI_NUMERICHOST);
+      puts(host);
+    }
+    for (tmp = notif.addrlist_IPv6; tmp != NULL; tmp = tmp->ai_next) {
+      printf("start connect IP6 \n");
+      fd = socket(tmp->ai_family, tmp->ai_socktype, tmp->ai_protocol);
+      if (fd == -1) {
+        continue;
+      }
+      if (connect(fd, tmp->ai_addr, (socklen_t)tmp->ai_addrlen) != 0) {
+        fprintf(stderr, "connect: %s\n", strerror(errno));
+      } else {
+        printf("connected to ");
+        getnameinfo(tmp->ai_addr, tmp->ai_addrlen, host, sizeof(host), NULL, 0,
+                  NI_NUMERICHOST);
+        puts(host);
+      }
+
+      break;
+    }
     break;
   default:
     puts("ERR");
     break;
   }
-  printf("addr=%s\n", notif.addr);
+
   t1.join();
   t2.join();
   puts("------------");
 }
 
 int main(int argc, char *argv[]) {
-  char *hostname;
+  const char *hostname;
   if (argc >= 2) {
     hostname = argv[1];
   } else {
-    hostname = "google.com";
+    hostname = "momoka.hongo.wide.ad.jp";
   }
   puts("case 1. AAAA is fast so send ipv6 syn");
   happyEyeball2(2000ms, 1000ms, hostname);
   puts("case 2. A is fast so send ipv4 syn");
   happyEyeball2(1000ms, 2000ms, hostname);
-  puts("case 2. A is a little fast so wait AAAA and send ipv6 syn");
+  puts("case 3. A is a little fast so wait AAAA and send ipv6 syn");
   happyEyeball2(1000ms, 1010ms, hostname);
 }
